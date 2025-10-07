808d515a765ca291d8290c5f81375d16b690bcf4
Ragipalyam Jaganmohan Reddy <ragipalyamjaganmohanjmr@gmail.com>
2025-10-07 10:24:53 +0530

    updated case sensitive words

--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getReviews,createReview } from "../controllers/reviewController.js";
-import authMiddleware from "../middleware/authMiddleware.js";
+import authMiddleware from "../middleware/authmiddleware.js";
 
 const router = express.Router();
 
